64123dea60cb2f68a49966d9bea3a4d7edd8f265
Wink Saville <wink@saville.com>
2020-11-18 17:19:30 -0800

    Initial Commit

---
+++ actions/docker-local-set-env-var/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -l
+
+echo "entrypoint.sh: name=$1"
+echo "entrypoint.sh: value=$2"
+
+# TODO: when act supports $GITHUB_ENV change to
+#echo "$1=$2" >> $GITHUB_ENV
+echo "::set-env name=$1::$2"
