cae7933cd5f0759b21be6f00d2939eade0140813
Wink Saville <wink@saville.com>
2020-11-19 16:59:49 -0800

    Simplification to match act-failing-python-cache
    This is succeeding but I was hoping it would fail!!

--- actions/docker-local-set-env-var/entrypoint.sh
+++ actions/docker-local-set-env-var/entrypoint.sh
@@ -3,6 +3,11 @@
 #echo "entrypoint.sh: name=$1"
 #echo "entrypoint.sh: value=$2"
 
+if [ "$1" == "" ]; then
+	echo "env-name is missing"
+	exit 1
+fi
+
 # TODO: when act supports $GITHUB_ENV change to
 #echo "$1=$2" >> $GITHUB_ENV
 echo "::set-env name=$1::$2"
